6a8c215061764309ea3d5b403aa211825f3e0b8f
unknown <lokih@ru.is>
2019-09-05 10:18:34 +0000

    third commit

--- max_int.py
+++ max_int.py
@@ -2,6 +2,7 @@ num_int = 0
 while num_int >= 0:
     previous_int = num_int
     num_int = int(input("Input a number: "))    # Do not change this line
-
+    if num_int >= pervious_int:
+        max_int = num_int
 print("The maximum is", max_int)    # Do not change this line
 
