91c4f70981439580c2db3534a1f9348847cfeb1f
unknown <lokih@ru.is>
2019-09-05 10:25:56 +0000

    added algorithm

--- max_int.py
+++ max_int.py
@@ -1,3 +1,8 @@
+# first we establish the values of num_int and previous_int
+# then we prompt the user to input a number inside a loop that runs while the input is not a negative number
+# then the program checks to see if the the latest input is higher than the previous and if so, saves it
+# the loop stops one a negative number is input, and it prints out the max_int
+
 num_int = 0
 while num_int >= 0:
     previous_int = num_int
