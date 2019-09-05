a659486b4cf8429e5633024ae75aef44ab8d5aff
unknown <lokih@ru.is>
2019-09-05 10:16:41 +0000

    second commit

--- max_int.py
+++ max_int.py
@@ -1,6 +1,7 @@
-
-num_int = int(input("Input a number: "))    # Do not change this line
-
+num_int = 0
+while num_int >= 0:
+    previous_int = num_int
+    num_int = int(input("Input a number: "))    # Do not change this line
 
 print("The maximum is", max_int)    # Do not change this line
 
